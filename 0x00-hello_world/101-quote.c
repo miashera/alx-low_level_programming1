634de8e54c0e20bf47ed5764d5fa367c3d0a5f8a
miashera <marywacera976@example.com>
2023-06-15 05:01:36 -0700

    101-intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - a unix program
@@ -6,6 +5,6 @@
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n",59);
 	return (1);
 }
