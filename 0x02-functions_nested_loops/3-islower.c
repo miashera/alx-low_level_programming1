22de4681ec98c2fa9a6f5cddd2bc98358a180d29
miashera <marywacera976@example.com>
2023-06-20 04:02:43 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - lowercase char
+ * @c: char checked
+ * Return: 1 lowercase char or 0 anything else
+ */
+
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+	
+	return (1);
+	}
+	return (0);
+}
