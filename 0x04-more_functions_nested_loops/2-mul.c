81bff35000a7b7c16f2747c79538c3a9a296765e
miashera <marywacera976@example.com>
2023-06-23 04:34:43 -0700

    task2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - mult two int
+ * @x: first num mult
+ * @y: second num mult by first num
+ * Return: result of mult btwn x param and y param
+ */
+int mul(int x, int y)
+
+{
+	return (x*y);
+}
