0880ae0d92cc2c4722950c214d138d5c3706c8bf
miashera <marywacera976@example.com>
2023-06-26 17:02:09 -0700

    tsk6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts2 - function should
+ * starting with first
+ * @str: input
+ * Return: print
+ */
+void puts(char *str)
+{
+	int longi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+
+	while (*y != '\0')
+	{
+		y++;
+		longi++;
+	}
+	t = longi - 1;
+	for (o = 0 ; o <= t ; o++)
+	{
+		if (o % 2 == 0)
+	{
+		_putchar(str[o]);
+	}
+	}
+	_putcha'\n';
+}
