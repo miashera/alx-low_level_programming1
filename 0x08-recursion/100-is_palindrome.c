265da3b9c2552d00fb46e2e95089a04c689159c8
miashera <marywacera976@example.com>
2023-07-05 12:02:31 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length
+ * @s: string
+ * Return: length strin
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * comparator - compares char of string
+ * @s: string
+ * @n1: small iterator
+ * @n2: big iterator
+ * Return: .
+ */
+int comparator(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 + 1)
+			return (1);
+		return (0 + comparator(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
+/**
+ * is_palindrome - detect
+ * @s: string
+ * Return: 1 or 0
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comparator(s, 0, _strlen_recursion(s) - 1));
+}
