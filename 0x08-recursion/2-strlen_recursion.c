53055526e32c0e3a6b8e29eb9c609cc5973557c9
miashera <marywacera976@example.com>
2023-07-05 10:43:07 -0700

    task

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion - calc
+ * @s: string
+ * Return: int value
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	s++;
+	return (_strlen_recursion(s) + 1);
+}
