916dfc0fa91c5f54ed40ecf661187e3428997a2c
miashera <marywacera976@example.com>
2023-08-02 18:27:03 -0700

    3

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - print the element
+ * @h: linked list
+ * Return: nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
